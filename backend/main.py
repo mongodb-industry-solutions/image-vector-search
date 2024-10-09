be2ff7c36bc6329ce63c8293e7bf240711935553
bereilhp <bereilhp@gmail.com>
2024-10-09 12:22:29 +0200

    Backend folder

---
+++ backend/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import APIRouter
+from image_search import image_search
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3456",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+router = APIRouter()
+
+@app.post("/imageSearch")
+async def find_similar_images(request: Request):
+    data = await request.json()
+    droppedImage = data.get("droppedImage") 
+    similar_documents = image_search(droppedImage)
+    return {"similar_documents": similar_documents}
+    
